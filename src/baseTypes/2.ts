c258fbc46066dad713b0df02b1d52ac73dc69b27
Dmytro Medvediev <dsisitkin@gmail.com>
2024-05-14 16:12:39 +0200

    Update 2.ts

--- src/baseTypes/2.ts
+++ src/baseTypes/2.ts
@@ -1,5 +1,13 @@
-let anything = -20;
+// let anything = -20;
+// anything = 'Text';
+// anything = {};
+
+// export { };
+
+// Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
+
+let anything: any = -20;
 anything = 'Text';
 anything = {};
 
-export {};
+export { };
