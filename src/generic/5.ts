4146ca6df5d39b614082f9b38b912b9bef061074
Dmytro Medvediev <dsisitkin@gmail.com>
2024-05-21 22:07:12 +0200

    Update 5.ts

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,18 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<K, V> {
+  key: K;
+  value: V;
 }
 
-export {};
+// Приклад використання
+const stringNumberPair: KeyValuePair<string, number> = {
+  key: "age",
+  value: 30,
+};
+
+const numberStringPair: KeyValuePair<number, string> = {
+  key: 1,
+  value: "one",
+};
